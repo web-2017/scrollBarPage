16da96c96ef13ee7e21962b9abb2789375a57e44
Mario Magomedov <webdevelope2017@gmail.com>
2018-03-25 15:33:52 +0300

    ddd

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = function () {
+  myScroll();
+}
+
+
+function myScroll() {
+  let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let documentHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (windowScroll / documentHeight) * 100;
+
+  document.getElementById('myBar').style.width = scrolled + '%';
+}
